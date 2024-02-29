63e4f675d6e3224f464fdae3f122f24b6d0134d1
henrique <henrique.florencio307@gmail.com>
2024-02-29 20:27:53 -0300

    resolvendo problema do auth token do github

--- src/components/main/carrossel/features/infinite-scrolling/repos.service.ts
+++ src/components/main/carrossel/features/infinite-scrolling/repos.service.ts
@@ -3,7 +3,7 @@ import { Octokit } from 'octokit';
 
 export async function getRepos(page: number) {
 
-    const ocktokit = new Octokit({ auth: "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" });
+    const ocktokit = new Octokit({ auth: process.env.GH_CREDENTIALS });
 
     const repos: Endpoints["GET /users/{username}/repos"]["response"] = await ocktokit.request("GET /users/{username}/repos", {
         per_page: 10,
